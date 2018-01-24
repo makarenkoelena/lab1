4a758f536b94e74bbcb15379085ac00b27daeab3
makarenkoelena <g00341964@gmit.ie>
2018-01-24 15:31:34 +0000

    str_len_both.js added

---
+++ str_len_both.js
@@ -0,0 +1,14 @@
+function str_len_both(value, spaces) {
+    if (spaces === void 0) { spaces = false; }
+    var num;
+    if (spaces) {
+        num = value.replace(/\s+/, "").length;
+    }
+    else {
+        num = value.length;
+    }
+    return num;
+}
+console.log("Function combined: String length with spaces and all is:" + str_len_both("test 1", false));
+console.log("Function combined: String length with spaces not included in the count: " + str_len_both("test 1", true));
+console.log("Function combined: String length with spaces and all is, not setting spaces parameter so will default to false: " + str_len_both("test 1"));
